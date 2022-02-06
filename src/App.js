438c23ad1bee0558da3af4a4939ee57fd8282b08
ahmad.mohammadi <a.mohammadi1026@gmail.com>
2022-02-07 00:56:12 +0330

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/home';
+import Login from './components/login';
+import Navbar from './components/navbar';
+import Register from './components/register';
 import Users from './components/users';
+
 class App extends Component {
     state = {}
     render() {
         return (
-            <div>
-                <Users />
-            </div>
+            <>
+                <Navbar />
+                <div className='container mt-3'>
+                    <Routes>
+                        <Route path='/users' element={<Users />} />
+                        <Route path='/login' element={<Login />} />
+                        <Route path='/register' element={<Register />} />
+                        <Route path='/' exact element={<Home />} />
+                    </Routes>
+                </div>
+            </>
         );
     }
 }
